c5b158f90b02fc7a9045ddf16d32b6349862a529
Venu Karanam <98816218+NarasimhaVenu@users.noreply.github.com>
2025-01-10 17:11:46 +0530

    commit

---
+++ node.js
@@ -0,0 +1,12 @@
+// Example JavaScript to handle icon clicks
+document.getElementById('upload').addEventListener('click', () => {
+    alert('Upload icon clicked!');
+});
+
+document.getElementById('download').addEventListener('click', () => {
+    alert('Download icon clicked!');
+});
+
+document.getElementById('view-template').addEventListener('click', () => {
+    alert('View Template icon clicked!');
+});
